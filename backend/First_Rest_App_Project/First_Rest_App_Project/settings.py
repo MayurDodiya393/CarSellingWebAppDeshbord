aed11c6b9563b5ff04386d6d3c087cf876fc454c
MayurDodiya393 <144420128+MayurDodiya393@users.noreply.github.com>
2025-01-03 14:53:41 +0530

    Update settings.py

--- backend/First_Rest_App_Project/First_Rest_App_Project/settings.py
+++ backend/First_Rest_App_Project/First_Rest_App_Project/settings.py
@@ -215,6 +215,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'msrdr393@gmail.com'
-EMAIL_HOST_PASSWORD = 'onon nyfq hmgu axps'
+EMAIL_HOST_USER = 'yourgmail@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 
